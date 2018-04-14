7bc457cf02e1307bad1eba5edc8f8f41866afd9e
Abhishek Singh <jonu.1504@gmail.com>
2018-04-14 08:05:03 +0000

    changes

--- config/route/index.js
+++ config/route/index.js
@@ -1,4 +1,5 @@
 'use strict';
 module.exports = (app, passport) => {
     app.use('/api', require('./route_v1')(passport));
+    app.get('/', (req, res) => res.status(200).json({ status: "OK" }));
 };
